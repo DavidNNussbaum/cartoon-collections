fe6be8c4912e71942bff98d2c82fb32d7dd3a4fa
DavidNNussbaum <brocknynj@aol.com>
2020-10-29 21:18:36 -0400

    commit tree 1c818efe5f772c42e78bb89cd6b0d12bd4b16066

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+  puts "#{index + 1}. #{item}"
+  end
+end
+ 
+
+
+
+def summon_captain_planet(array)
+  array2 = array.map { |n| n.capitalize}  
+  array3 = array2.map { |n| n + "!"}
+  return array3
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def long_planeteer_calls(array)
+   return array.any? { |item| item.length > 4}
+  end
+     
+  
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |a| a == "cheddar" || a == "gouda" || a == "camembert" }
+  # return cheese_types & array
+  # array.each do |item|
+  # cheese_types.each do |content|
+  #   item == content
+  # return array.find do { |item| item in? cheese_types}
+  # 'Unicorn'.in? (['Cat ',' Dog ',' Bird '])
+  
 end
